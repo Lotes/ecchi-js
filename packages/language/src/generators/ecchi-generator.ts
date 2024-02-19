bdfd2d560d761fc032d589dc1b43dc55f2a2db70
Markus  Rudolph <markhoernchen@gmx.de>
2024-02-19 21:36:01 +0100

    Fix subject generation

--- packages/language/src/generators/ecchi-generator.ts
+++ packages/language/src/generators/ecchi-generator.ts
@@ -1,6 +1,6 @@
 import { URI, assertUnreachable } from "langium";
 import { EcchiServices } from "../ecchi-module.js";
-import { Model, TypeReference, isInterfaceDefinition } from "../generated/ast.js";
+import { InterfaceDefinition, Model, SubjectDefinition, TypeReference, UserDefinition, UserMemberDefinition, isInterfaceDefinition, isSubjectDefinition, isUserDefinition } from "../generated/ast.js";
 import { NestedSetElement } from "@ecchi-js/core";
 
 interface TypeTree {
@@ -25,15 +25,74 @@ export class EcchiGenerator {
     }
   }
   private generateFromModel(model: Model) {
-   const dts = `${this.generateImports()}
+    const { interfaces, nestedSets, trees } = this.getInterfaces(model);
+    const dts = `${this.generateImports()}
 
-${this.generateTypes(model)}
+${this.generateTypes(interfaces, trees)}
 
-${this.generateReflection(model)}
+${this.generateReflection(interfaces, nestedSets)}
+
+${this.generateUser(model.elements.filter(isUserDefinition))}
+
+${this.generateSubjectActions(model.elements.filter(isUserDefinition).flatMap(u => u.members))}
 `;
     return dts;
   }
-  private generateReflection(model: Model) {
+  generateUser(users: UserDefinition[]) {
+    return `export type $UserType = ${users[0].type};`;
+  }
+  private getSubjectActions(member: SubjectDefinition) {
+    const parents: Record<string, string|undefined> = Object.fromEntries(member.members.map(m => [m.name, m.superAction?.ref?.name] as const));
+    const children: Record<string, string[]> = Object.fromEntries(Object.entries(parents).map(([name]) => [name, [] as string[]] as const));
+    for (const [child, parent] of Object.entries(parents).filter(([_, parent]) => parent !== undefined)) {
+      children[parent!].push(child);
+    }
+    return {
+      parents,
+      children
+    };
+  }
+  generateSubjectActions(members: UserMemberDefinition[]) {
+    const subjects = members.filter(isSubjectDefinition);
+    return `export const $SubjectActions = {
+  ${subjects.map(subject => {
+    const { children, parents } = this.getSubjectActions(subject);
+    const type = Object.keys(parents).map(a => `'${a}'`).join('|');
+    const nestedSets = new Map<string, NestedSetElement>();
+    let counter = 0;
+    function visit(node: string) {
+      const left = counter++;
+      for (const child of children[node]) {
+        visit(child);
+      }
+      const right = counter++;
+      nestedSets.set(node, [
+        left,
+        right
+      ]);
+    }
+    for (const node of Object.entries(parents).filter(([_, parent]) => parent === undefined).map(([action]) => action)){
+      visit(node);
+    }
+    return `${subject.name}: ["${subject.type.ref?.name}", new SubjectActions<${type}>({
+    ${Object.keys(parents).map((action, index) => {
+        const [left, right] = nestedSets.get(action)!;
+        return `${action}: [[${left}, ${right}],  ${index}]`;
+      }).join(',\n    ')}
+  })]`; 
+  }).join('\n  ')}
+} satisfies SubjectActionsBase<$Types>;    
+`;
+  }
+  private generateReflection(interfaces: InterfaceDefinition[], nestedSets: Map<string, NestedSetElement>) {
+    return `export const $Reflection = new Reflection<$Types>({
+  ${interfaces.map(iface => {
+  const set = nestedSets.get(iface.name)!;
+  return `${iface.name}: [${set[0]},  ${set[1]}],`
+}).join('\n  ')}
+});`;
+  }
+  private getInterfaces(model: Model) {
     const map = new Map<string, TypeTree>();
     const nestedSets = new Map<string, NestedSetElement>();
     const interfaces = model.elements.filter(isInterfaceDefinition);
@@ -46,7 +105,7 @@ ${this.generateReflection(model)}
     }
     for (const iface of interfaces) {
       const node = map.get(iface.name)!;
-      if(iface.superInterface) {
+      if (iface.superInterface) {
         const superNode = map.get(iface.superInterface.ref!.name)!;
         superNode.children.push(node);
       } else {
@@ -68,21 +127,23 @@ ${this.generateReflection(model)}
     for (const node of roots) {
       visit(node);
     }
-    return `export const $Reflection = new Reflection<$Types>({
-  ${interfaces.map(iface => {
-  const set = nestedSets.get(iface.name)!;
-  return `${iface.name}: [${set[0]},  ${set[1]}],`
-}).join('\n  ')}
-});`;
+    return { interfaces, nestedSets, trees: map };
   }
-  public generateTypes(model: Model) {
-    return model.elements.filter(isInterfaceDefinition).map(type => `interface ${type.name}${type.superInterface ? ` extends ${type.superInterface.ref?.name}` : ''} {
-  $type: "${type.name}";
+
+  public generateTypes(interfaces: InterfaceDefinition[], trees: Map<string, TypeTree>) {
+    function visit(name: string) {
+      const subTypes: string = trees.get(name)!.children.map(c => visit(c.$type)).join('|');
+      return `"${name}"${subTypes.length > 0 ? `|${subTypes}` : ''}`;
+    }
+    return interfaces.map(type => { 
+      return `interface ${type.name}${type.superInterface ? ` extends ${type.superInterface.ref?.name}` : ''} {
+  $type: ${visit(type.name)};
   ${type.members.map(member => `${member.name}: ${this.generateTypeReference(member.type)};`).join('\n  ')}
-}`).join('\n  ')+`
+}`;
+}).join('\n')+`
 
 export type $Types = {
-  ${model.elements.filter(isInterfaceDefinition).map(type => {
+  ${interfaces.map(type => {
   return `${type.name}: ${type.name},`;
 }).join('\n  ')}
 }`;
