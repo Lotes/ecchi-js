a6d9669eec6f8dadbe1753a3c87ec810a3878b29
Markus  Rudolph <markhoernchen@gmx.de>
2024-02-27 20:00:37 +0100

    Fix map usage

--- packages/language/src/ecchi-generator.ts
+++ packages/language/src/ecchi-generator.ts
@@ -1,10 +1,10 @@
 import { URI, assertUnreachable } from "langium";
 import { EcchiServices, createEcchiServices } from "./ecchi-module.js";
-import { ConceptDefinition, Model, SubjectDefinition, TypeReference, isSubjectDefinition } from "./generated/ast.js";
+import { ConceptDefinition, Model, SubjectDefinition, TypeReference } from "./generated/ast.js";
 import { NestedSetElement } from "@ecchi-js/core";
 import { EmptyFileSystem } from "langium";
 import { readFile } from "fs/promises";
-import { Tree, buildDomain } from "./ecchi-model.js";
+import { SubjectData, Tree, buildDomain } from "./ecchi-model.js";
 
 export async function generate(fileName: string) {
   const services = createEcchiServices(EmptyFileSystem);
@@ -29,8 +29,8 @@ export class EcchiGenerator {
     }
   }
   private generateFromModel(model: Model) {
-    const { concepts, user } = buildDomain(model);
-    const dts = `${this.generateImports()}
+    const { concepts, user, subjects } = buildDomain(model);
+    const typescript = `${this.generateImports()}
 
 ${this.generateTypes(concepts.instances, concepts.map)}
 
@@ -38,48 +38,21 @@ ${this.generateReflection(concepts.instances, concepts.hierarchy)}
 
 ${this.generateUser(user)}
 
-${this.generateSubjectActions(model.elements.filter(isSubjectDefinition))}
+${this.generateSubjectActions(subjects.instances, subjects.map)}
 `;
-    return dts;
+    return typescript;
   }
   generateUser(user: ConceptDefinition | undefined) {
     return `export type $UserType = ${user?.name};`;
   }
-  private getSubjectActions(member: SubjectDefinition) {
-    const parents: Record<string, string|undefined> = Object.fromEntries(member.members.map(m => [m.name, m.superAction?.ref?.name] as const));
-    const children: Record<string, string[]> = Object.fromEntries(Object.entries(parents).map(([name]) => [name, [] as string[]] as const));
-    for (const [child, parent] of Object.entries(parents).filter(([_, parent]) => parent !== undefined)) {
-      children[parent!].push(child);
-    }
-    return {
-      parents,
-      children
-    };
-  }
-  generateSubjectActions(subjects: SubjectDefinition[]) {
+  generateSubjectActions(instances: SubjectDefinition[], map: Map<string, SubjectData>) {
     return `export const $SubjectActions = {
-  ${subjects.map(subject => {
-    const { children, parents } = this.getSubjectActions(subject);
-    const type = Object.keys(parents).map(a => `'${a}'`).join('|');
-    const nestedSets = new Map<string, NestedSetElement>();
-    let counter = 0;
-    function visit(node: string) {
-      const left = counter++;
-      for (const child of children[node]) {
-        visit(child);
-      }
-      const right = counter++;
-      nestedSets.set(node, [
-        left,
-        right
-      ]);
-    }
-    for (const node of Object.entries(parents).filter(([_, parent]) => parent === undefined).map(([action]) => action)){
-      visit(node);
-    }
+  ${instances.map(subject => {
+    const {parents, hierarchy: nestedSets, instances} = map.get(subject.name)!;
+    const type = [...parents.keys()].map(a => `'${a}'`).join('|');
     return `${subject.name}: ["${subject.type?.ref?.name}", new SubjectActions<${type}>({
-    ${Object.keys(parents).map((action, index) => {
-        const [left, right] = nestedSets.get(action)!;
+    ${[...instances.entries()].map(([action, tree], index) => {
+        const [left, right] = nestedSets.get(tree.content)!;
         return `${action}: [[${left}, ${right}],  ${index}]`;
       }).join(',\n    ')}
   })]`; 
